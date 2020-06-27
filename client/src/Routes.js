23abcec522d161b9f256521125354d4ab66ec347
u18164022 <62012585+u18164022@users.noreply.github.com>
2020-06-27 16:39:20 +0200

    UI navbar design
    implementatiion for navigation bar

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -6,24 +6,24 @@ import {
 	Link
 } from "react-router-dom";
 import Home from './Home'
+import User from './User'
 import ProjectPage from './ProjectPage'
+import {Navbar, Nav} from 'react-bootstrap'
 
 
 class Routes extends Component {  
 	render() {
 		return (
 			<Router>
-				<div>
-					<nav>
-					<ul>
-						<li>
-						<Link to="/">Home</Link>
-						</li>
-						<li>
-						<Link to="/project">Projects</Link>
-						</li>
-					</ul>
-					</nav>
+					<Navbar bg="dark" variant="dark">
+						<Nav className="mr-auto" >
+						<Nav.Link href="/">Home</Nav.Link>
+						<Nav.Link href="/project">Projects</Nav.Link>
+						</Nav>
+						<Nav>
+						<Nav.Link className="form-inline"  href="/user">Login</Nav.Link>
+						</Nav>
+					</Navbar>
 
 					{/* A <Switch> looks through its children <Route>s and
 						renders the first one that matches the current URL. */}
@@ -34,8 +34,10 @@ class Routes extends Component {
 					<Route path="/">
 						<Home />
 					</Route>
+					<Route path="/user">
+						<User />
+					</Route>
 					</Switch>
-				</div>
 			</Router>
 		);
 	}
--- client/src/User.js
+++ client/src/User.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
-class Home extends Component{
+class User extends Component{
     render(){
         return(
             <React.Fragment>
+                <h1>Under Construction</h1>
             </React.Fragment>
         )
     }
 }
 
-export default Home;
+export default User;
