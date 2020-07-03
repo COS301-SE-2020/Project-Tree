b6af88f4bc5bad869a8160dd92e76db0d46b2628
u18164022 <62012585+u18164022@users.noreply.github.com>
2020-07-03 13:03:31 +0200

    Dependency creation buttons UI
    Colour and placement

--- client/src/Graph/Graph.js
+++ client/src/Graph/Graph.js
@@ -238,14 +238,14 @@ class Graph extends React.Component {
                 <Container className="text-center py-2">
                     <Row>
                         <Col></Col>
-                        <Col>
+                        <Col xs={6}>
                         <Row>
-                            {dependency != null ? <Col className="text-center"><Button variant="outline-secondary" block size="sm">{dependency}</Button></Col> : null}
-                            {this.state.source != null ? <Col className="text-center"><Button onClick={this.clearDependency} variant="outline-secondary" block size="sm">X</Button></Col> : null}
+                            {dependency != null ? <Col className="text-center" xs={5}><Button variant="outline-secondary" block size="sm">{dependency}</Button></Col> : null}
+                            {this.state.source != null ? <Col className="text-center"><Button onClick={this.clearDependency} variant="danger" block size="sm"><i className="fa fa-close"></i></Button></Col> : null}
                             <Col className="text-center"><Button variant="outline-secondary" block size="sm" onClick={this.zoomIn}><i className="fa fa-search-plus"></i></Button></Col>
                             <Col className="text-center"><Button variant="outline-secondary" block size="sm" onClick={this.zoomOut}><i className="fa fa-search-minus"></i></Button></Col>
                             <Col className="text-center"> <Button variant="dark" size="sm" block onClick={this.resetZoom}><i className="fa fa-repeat"></i></Button></Col>
-                            </Row>
+                        </Row>
                         </Col>
                         <Col></Col>
                     </Row>
