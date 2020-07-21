d402b70ada7124b9da15b6509f8357bcac9743f0
Damian Venter <u18076514@tuks.co.za>
2020-07-21 15:34:52 +0200

    Testing CodingTrain solution

--- api/DB.js
+++ api/DB.js
@@ -1,19 +1,12 @@
-const aws = require('aws-sdk');
+require('dotenv').config()
 
-let s3 = new aws.S3({
-  accessKeyId: process.env.S3_KEY,
-  secretAccessKey: process.env.S3_SECRET
-});
-console.log(s3)
+var neo4j = require('neo4j-driver');
+var driver = neo4j.driver('bolt://hobby-mhcikakdabfpgbkehagladel.dbs.graphenedb.com:24786', neo4j.auth.basic(process.env.DB_USERNAME, process.env.DB_PASSWORD), {encrypted: 'ENCRYPTION_ON'});
 
-// var neo4j = require('neo4j-driver');
-// let username = process.env.
-// var driver = neo4j.driver('bolt://hobby-mhcikakdabfpgbkehagladel.dbs.graphenedb.com:24786', neo4j.auth.basic("apiuser", "b.itTi1EW0Ydq8.TC6VgDMZxKtzSkGD"), {encrypted: 'ENCRYPTION_ON'});
-
-// function getSession()
-// {
-//    return driver.session();
-// }
+function getSession()
+{
+   return driver.session();
+}
 
 module.exports =
 {
