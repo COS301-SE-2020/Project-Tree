03f0a5f098361071102259cdd310b01e764cf42d
Damian Venter <u18076514@tuks.co.za>
2020-07-30 08:41:23 +0200

    Added modal functionality on project press
    Modal displays project info

--- clientMobile/Project/ProjectList.js
+++ clientMobile/Project/ProjectList.js
@@ -1,41 +1,51 @@
 import React, { Component } from 'react';
-import { View } from 'react-native'
-import { Container, Header, Tab, Tabs, TabHeading, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, StyleProvider } from 'native-base';
+import { Container, Header, Tab, Tabs, TabHeading, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, StyleProvider } from 'native-base';
+import { Alert, Modal, StyleSheet, Text, View, TouchableHighlight } from "react-native";
 import buttonStyling from '../native-base-theme/variables/buttonStylingProjList';
 import getTheme from '../native-base-theme/components';
+import ProjectModal from './ProjectModal'
 
-class ProjectListPage extends Component{
+class ProjectListPage extends Component {
     constructor(props){
         super(props);
-        this.state = {projects:null, project:null};
+        this.state = {projects:null, project:null, modalVisible:false, tableData: null };
         this.toggleActionSheet = this.toggleActionSheet.bind(this);
+        this.setModalVisible = this.setModalVisible.bind(this)
         // this.setProjectInfo = this.setProjectInfo.bind(this); 
     }
-
-    toggleActionSheet(selectedProject){
-        console.log(selectedProject.name);
+    
+    toggleActionSheet = (selectedProject) => {
         this.setState({project: selectedProject});
+        this.setModalVisible(true);
     }
   
+    setModalVisible = (visible) => {
+        this.setState({ modalVisible: visible });
+    }
+
     async componentDidMount(){
-      const response = await fetch('http://projecttree.herokuapp.com/project/get',{
-          method: 'POST',
-          headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          },
-          body:null,
-      });
-      const body = await response.json();
-      this.setState({projects:body.nodes});
+        const response = await fetch('http://projecttree.herokuapp.com/project/get',{
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body:null,
+        });
+        const body = await response.json();
+        this.setState({projects:body.nodes});
     }
   
-    render(){
-      return(
-        <Content padder style={{ padding: 5 }}>
-            <ProjectList projects={this.state.projects} toggleActionSheet={this.toggleActionSheet}/>
-        </Content>
-      )
+    render() {
+        const { modalVisible } = this.state;
+        return (
+            <Content>
+                <View padder style={{ padding: 5 }}>
+                    {this.state.project !== null ? <ProjectModal project={this.state.project} modalVisible={modalVisible} setModalVisible={this.setModalVisible}/> : null}
+                    <ProjectList projects={this.state.projects} toggleActionSheet={this.toggleActionSheet}/>
+                </View>
+            </Content>
+        );
     }
 }
   
@@ -62,5 +72,49 @@ class ProjectList extends React.Component{
         );
     }
 }
+
+const styles = StyleSheet.create({
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        shadowColor: "#000",
+        shadowOffset: {
+        width: 0,
+        height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+    },
+    openButton: {
+        backgroundColor: "#F194FF",
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+    },
+    textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: "center"
+    },
+    container: { flex: 1, paddingTop: 30, backgroundColor: '#fff', width: "100%" },
+    head: {  height: 40,  backgroundColor: '#f1f8ff', width: 200 },
+    wrapper: { flexDirection: 'row' },
+    title: { flex: 1, backgroundColor: '#f6f8fa' },
+    row: {  height: 40  },
+    text: { margin: 6, textAlign: 'center' }
+});
 
 export default ProjectListPage;
---
+++ clientMobile/Project/ProjectModal.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import { Alert, Modal, StyleSheet, Text, TouchableHighlight, View, Button } from "react-native";
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+
+class ProjectModal extends Component {
+    render() {
+        const tableData = []
+        const tempArr = []
+        for(let x = 0; x < this.props.project.permissions.length; x++){
+            if(this.props.project.permissions[x] === true){
+                tempArr.push("x")
+            }
+            else{
+                tempArr.push("")
+            }
+        }
+
+        while(tempArr.length) tableData.push(tempArr.splice(0,3));
+
+        return (
+            <Modal animationType="slide" transparent={true} visible={this.props.modalVisible} onRequestClose={() => { Alert.alert("Modal has been closed."); }}>
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>{this.props.project.name}</Text>
+                        <Text style={styles.modalText}>{this.props.project.description}</Text>
+                        <View style={styles.container}>
+                            <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+                                <Row data={["","Create","Delete","Update"]} style={styles.head} textStyle={styles.text}/>
+                                <TableWrapper style={styles.wrapper}>
+                                    <Col data={["Package Manager","Responsible Person","Resource"]} style={styles.title} heightArr={[40,40,40]} textStyle={styles.text}/>
+                                    <Rows data={ tableData } flexArr={[1, 1, 1]} style={styles.row} textStyle={styles.text}/>
+                                </TableWrapper>
+                            </Table>
+                        </View>
+                        <TouchableHighlight style={{ ...styles.openButton, backgroundColor: "#2196F3" }} onPress={() => { this.props.setModalVisible(!this.props.modalVisible); }}>
+                            <Text style={styles.textStyle}>Hide Modal</Text>
+                        </TouchableHighlight>
+                    </View>
+                </View>
+            </Modal>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+        width: 0,
+        height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+    },
+    openButton: {
+        backgroundColor: "#F194FF",
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+    },
+    textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: "center"
+    },
+    container: { flex: 1, paddingTop: 30, backgroundColor: '#fff', width: "100%" },
+    head: {  height: 40,  backgroundColor: '#f1f8ff', width: 200 },
+    wrapper: { flexDirection: 'row' },
+    title: { flex: 1, backgroundColor: '#f6f8fa' },
+    row: {  height: 40  },
+    text: { margin: 6, textAlign: 'center' }
+});
+
+export default ProjectModal;
