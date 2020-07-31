f93c67ceef6d5d5207dc70e2deff5c4e4240d238
u18164022 <62012585+u18164022@users.noreply.github.com>
2020-07-31 22:21:12 +0200

    revamp webpage design
    Complete revamp

--- clientWeb/src/SideBar.js
+++ clientWeb/src/SideBar.js
@@ -30,7 +30,7 @@ class SideBar extends React.Component {
         <Button
           key={project.id}
           onClick={() => {this.handleClick(project);}}
-          variant="secondary"
+          style={{fontFamily:"arial black", backgroundColor: "#EEBB4D", borderColor:"#EEBB4D"}}
           size="sm"
           block
         >
@@ -40,7 +40,7 @@ class SideBar extends React.Component {
     });
 
     return (
-      <Container>
+      <Container className="py-2">
         <Row> {listItems} </Row>
       </Container>
     );
