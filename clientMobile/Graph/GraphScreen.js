3868fbed47a9d1a7fda58fac1eee0110db2b93e2
Will-A897 <61409127+Will-A897@users.noreply.github.com>
2020-08-05 13:03:37 +0200

    Basic communication between react-native and webview

--- clientMobile/Graph/GraphScreen.js
+++ clientMobile/Graph/GraphScreen.js
@@ -4,28 +4,18 @@ import { Container, Header, Picker ,Textarea, Tab, Tabs, TabHeading, Label, Form
 import { WebView } from 'react-native-webview';
 import ProjectModal from './GraphProjectModal';
 
-var htmlCode = '<p>Hello</p>'
 class GraphScreen extends Component{
     render(){
-        // console.log(webTest)
-        // if(this.props.project === null){
-        //     return(
-        //         <View>
-        //             <Text>
-        //                 Please select a project to view from the home page
-        //             </Text>
-        //         </View>
-        //     )
-        // }
-
         return (
             <View style={styles.container}>
                 <View style={{flex:30}}>
-                    <WebView source={{html : htmlCode}} />
+                    <WebView 
+                        source={{ uri: 'http://10.0.2.2:5000/mobile' }} 
+                        onMessage={event => {
+                            alert(event.nativeEvent.data);
+                        }}
+                    />
                 </View>
-                {/* <View style={{flex:1}}>
-                    <ProjectModal project={this.props.project}/>
-                </View> */}
             </View>
         );
     }
