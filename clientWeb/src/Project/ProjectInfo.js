e8c875043c3dd0eb25967bd77cde19f371d575eb
u18164022 <62012585+u18164022@users.noreply.github.com>
2020-08-12 19:10:42 +0200

    Notification button implementation
    Form to send notification message

--- clientWeb/src/Project/ProjectInfo.js
+++ clientWeb/src/Project/ProjectInfo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Table, Button, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
+import SendProjectNotification from "../Notifications/SendProjectNotification";
 import UpdateProject from "./UpdateProject";
 import DeleteProject from "./DeleteProject";
 
@@ -64,11 +64,10 @@ class ProjectInfo extends React.Component{
             </Col>
           </Row>
           <Row className="align-items-center py-2">
-            <Col></Col>
             <Col xs={6} className="text-center">
               <UpdateProject project={this.props.project} setProject={project => {this.props.setProject(project)}}/>
             </Col>
-            <Col></Col>
+            <Col> <SendProjectNotification/> </Col>
           </Row>
         </Container>
       </React.Fragment>
