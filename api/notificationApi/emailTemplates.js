a99df87f8862fc5602e068a9ec63f2153e96c5be
u18164022 <62012585+u18164022@users.noreply.github.com>
2020-08-15 10:50:31 +0200

    Email templates for different email notifications
    Html for emails

--- api/notificationApi/emailTemplates.js
+++ api/notificationApi/emailTemplates.js
@@ -18,12 +18,23 @@ function createHtmlEmail(from_name, taskName, projectName, _to, message){
                 </style>
                 </head>
             
-                <body>
+                <body> //Project-Wide
                     <img src="https://cdn.discordapp.com/attachments/713307810942681089/738461435297923142/Project_Tree.png" alt="Project-Tree">
-                    <h2>You have been joined to a new project, `+ projectName +`.<br/> <a href="https://projecttree.herokuapp.com/">Go check it out!</a></h2>
-                    <h2>You have been assigned to a new task, `+ taskName +`.<br/> <a href="https://projecttree.herokuapp.com/">Go check it out!</a></h2>
-                    <h2>New notification from `+ from_name +` for project `+ projectName+ ` regarding your task `+ taskName +`.<br/>`+ message +` <br/> <a href="https://projecttree.herokuapp.com/">Explore</a></p></h2>
+                    <h2> New notification from `+ from_name +` for project `+ projectName +`.<br/>`+ message +` <br/> <a href="https://projecttree.herokuapp.com/">Explore</a></p></h2>
                     
+                </body>
+                <body> //Task-Wide
+                    <img src="https://cdn.discordapp.com/attachments/713307810942681089/738461435297923142/Project_Tree.png" alt="Project-Tree">
+                    <h2> New notification from `+ from_name +` for project `+ projectName+ ` regarding your task `+ taskName +`.<br/>`+ message +` <br/> <a href="https://projecttree.herokuapp.com/">Explore</a></p></h2>
+                    
+                </body>
+                <body> //Automated Project assignment
+                    <img src="https://cdn.discordapp.com/attachments/713307810942681089/738461435297923142/Project_Tree.png" alt="Project-Tree">
+                    <h2>You have been added to a new project, `+ projectName +`.<br/> <a href="https://projecttree.herokuapp.com/">Go check it out!</a></h2>                    
+                </body>
+                <body> //Automated task assignment
+                    <img src="https://cdn.discordapp.com/attachments/713307810942681089/738461435297923142/Project_Tree.png" alt="Project-Tree">
+                    <h2> You have been assigned to a new task, `+ taskName +` in your project `+ projectName +`.<br/> <a href="https://projecttree.herokuapp.com/">Go check it out!</a></h2>                    
                 </body>
             </html>
                 `
