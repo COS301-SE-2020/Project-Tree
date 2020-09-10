f8670941b35373ec993ab87d34399439be7075a0
u18164022 <62012585+u18164022@users.noreply.github.com>
2020-09-10 18:16:05 +0200

    Carousel resizing
    Make bigger

--- clientWeb/src/About.js
+++ clientWeb/src/About.js
@@ -1,40 +1,48 @@
 import React, { Component } from "react";
-import { Carousel } from "react-bootstrap";
+import { Carousel, Container, Row, Col } from "react-bootstrap";
 import "./About.scss";
 import logo from "./Images/Logo.png";
 
 class About extends Component {
   render() {
     return (
-      <Carousel className="border rounded">
-        <Carousel.Item>
-          <img src={logo} alt="Logo" />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={logo} alt="Logo" />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={logo} alt="Logo" />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={logo} alt="Logo" />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={logo} alt="Logo" />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <Container>
+        <Row>
+            <Col>
+              <Carousel className="border rounded">
+              <Carousel.Item>
+                <img src={logo} alt="Logo" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={logo} alt="Logo" />
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={logo} alt="Logo" />
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={logo} alt="Logo" />
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={logo} alt="Logo" />
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+            </Col>
+          </Row>
+      </Container>
+      
+       
     );
   }
 }
