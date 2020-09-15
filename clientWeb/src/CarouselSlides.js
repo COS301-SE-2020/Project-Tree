3821fcdd8d14e05ca9360f00e54a015ca52a8511
u18164022 <62012585+u18164022@users.noreply.github.com>
2020-09-15 16:15:07 +0200

    Carousel images

--- clientWeb/src/CarouselSlides.js
+++ clientWeb/src/CarouselSlides.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import { Carousel, Container, Row, Col } from "react-bootstrap";
 import "./Carousel.scss";
-import logo from "./Images/Logo.png";
+import Dashboard from "./Images/Dashboard.png";
+import Graph from "./Images/Graph.png";
+import Mobile from "./Images/Mobile.png";
+import Notification from "./Images/Notification.png";
+import Calendar from "./Images/Calendar.png";
 
 class CarouselSlides extends Component {
   render() {
@@ -11,30 +15,28 @@ class CarouselSlides extends Component {
             <Col className="text-center align-items-center">
               <Carousel className="border rounded align-items-center">
               <Carousel.Item>
-                <img src={logo} alt="Logo" />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img src={logo} alt="Logo" />
+                <img src={Dashboard} alt="Logo" className="carouselSlides" />
                 <Carousel.Caption>
-                  <h3>Second slide label</h3>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
-                <img src={logo} alt="Logo" />
+                <img src={Graph} alt="Graph" className="carouselSlides" />
                 <Carousel.Caption>
-                  <h3>Third slide label</h3>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
-                <img src={logo} alt="Logo" />
+              <img src={Mobile} alt="Mobile" className="carouselSlides" />
                 <Carousel.Caption>
-                  <h3>Third slide label</h3>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
-                <img src={logo} alt="Logo" />
+                <img src={Notification} alt="Notification" className="carouselSlides" />
                 <Carousel.Caption>
-                  <h3>Third slide label</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={Calendar} alt="Calendar" className="carouselSlides" />
+                <Carousel.Caption>
                 </Carousel.Caption>
               </Carousel.Item>
             </Carousel>
