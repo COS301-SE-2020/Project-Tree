b8aaf48b635ec79dd4806dcd285e7ce2a3d8195c
Brandon Robinson <u18021353@tuks.co.za>
2020-10-02 13:04:15 +0200

    added create project start and end date

--- api/projectApi/projectQueries.js
+++ api/projectApi/projectQueries.js
@@ -38,6 +38,9 @@ async function createProject(req, res) {
       ? (cp_r_Update = true)
       : (cp_r_Update = false);
 
+    let startDate = `${req.body.cp_StartDate}T${req.body.cp_StartTime}`;
+    let endDate = `${req.body.cp_EndDate}T${req.body.cp_EndTime}`;
+
     db.getSession()
       .run(
         `
@@ -46,7 +49,9 @@ async function createProject(req, res) {
           CREATE(n:Project {
               name:"${req.body.cp_Name}", 
               description:"${req.body.cp_Description}", 
-              projManCT:true, 
+              startDate: datetime("${startDate}"),
+              endDate: datetime("${endDate}"),
+              projManCT:true,
               projManDT:true, 
               projManUT:true, 
               packManCT:${cp_pm_Create}, 
@@ -69,6 +74,12 @@ async function createProject(req, res) {
           id: result.records[0]._fields[0].identity.low,
           name: result.records[0]._fields[0].properties.name,
           description: result.records[0]._fields[0].properties.description,
+          startDate: up.datetimeToString(
+            result.records[0]._fields[0].properties.startDate
+          ),
+          endDate: up.datetimeToString(
+            result.records[0]._fields[0].properties.endDate
+          ),
           permissions: [
             result.records[0]._fields[0].properties.packManCT,
             result.records[0]._fields[0].properties.packManDT,
@@ -156,6 +167,10 @@ async function updateProject(req, res) {
     req.body.up_r_Update != undefined
       ? (up_r_Update = true)
       : (up_r_Update = false);
+      
+    let startDate = `${req.body.up_StartDate}T${req.body.up_StartTime}`;
+    let endDate = `${req.body.up_EndDate}T${req.body.up_EndTime}`;
+
     db.getSession()
       .run(
         `
@@ -164,6 +179,8 @@ async function updateProject(req, res) {
           SET a += {
               name:"${req.body.up_name}",
               description:"${req.body.up_description}",
+              startDate: datetime("${startDate}"),
+              endDate: datetime("${endDate}"),
               projManCT:true, 
               projManDT:true, 
               projManUT:true, 
@@ -186,6 +203,12 @@ async function updateProject(req, res) {
           id: result.records[0]._fields[0].identity.low,
           name: result.records[0]._fields[0].properties.name,
           description: result.records[0]._fields[0].properties.description,
+          startDate: up.datetimeToString(
+            result.records[0]._fields[0].properties.startDate
+          ),
+          endDate: up.datetimeToString(
+            result.records[0]._fields[0].properties.endDate
+          ),
           permissions: [
             result.records[0]._fields[0].properties.packManCT,
             result.records[0]._fields[0].properties.packManDT,
@@ -230,6 +253,12 @@ async function getProjects(req, res) {
             id: record._fields[0].identity.low,
             name: record._fields[0].properties.name,
             description: record._fields[0].properties.description,
+            startDate: up.datetimeToString(
+              record._fields[0].properties.startDate
+            ),
+            endDate: up.datetimeToString(
+              record._fields[0].properties.endDate
+            ),
             permissions: [
               record._fields[0].properties.packManCT,
               record._fields[0].properties.packManDT,
@@ -264,6 +293,12 @@ async function getProjects(req, res) {
             id: record._fields[0].identity.low,
             name: record._fields[0].properties.name,
             description: record._fields[0].properties.description,
+            startDate: up.datetimeToString(
+              record._fields[0].properties.startDate
+            ),
+            endDate: up.datetimeToString(
+              record._fields[0].properties.endDate
+            ),
             permissions: [
               record._fields[0].properties.packManCT,
               record._fields[0].properties.packManDT,
