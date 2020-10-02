d7b785582967bd7de4bc4548e66dba41b0dbda89
Brandon Robinson <u18021353@tuks.co.za>
2020-10-02 14:03:26 +0200

    added start and enddate info to project page

--- clientWeb/src/Project/ProjectInfo.js
+++ clientWeb/src/Project/ProjectInfo.js
@@ -64,7 +64,35 @@ class ProjectInfo extends React.Component {
               </Row>
             </Col>
           </Row>
-          <Row className="align-items-center py-2">
+          <Row className="align-items-center pt-1">
+            <Col
+              className="align-items-center text-center"
+              style={{ fontSize: "20px", }}
+            >
+              Start Date and Time
+            </Col>
+            <Col
+              className="align-items-center text-center"
+              style={{ fontSize: "20px", }}
+            >
+              End Date and Time
+            </Col>
+          </Row>
+          <Row className="align-items-center">
+            <Col
+              className="align-items-center text-center"
+              style={{ fontSize: "20px", }}
+            >
+              {`${this.props.project.projectInfo.startDate.substring(0, 10)} ${this.props.project.projectInfo.startDate.substring(11, 16)}`}
+            </Col>
+            <Col
+              className="align-items-center text-center"
+              style={{ fontSize: "20px", }}
+            >
+              {`${this.props.project.projectInfo.endDate.substring(0, 10)} ${this.props.project.projectInfo.endDate.substring(11, 16)}`}
+            </Col>
+          </Row>
+          <Row className="align-items-center py-1">
             <Col
               className="align-items-center text-center"
               style={{ fontSize: "20px", wordWrap: "break-word" }}
