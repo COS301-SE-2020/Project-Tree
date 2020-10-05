e4e1f69c6cae97c0cb5bb596db47da5e22315b63
Brandon Robinson <u18021353@tuks.co.za>
2020-10-05 11:00:32 +0200

    fiexed issue with update project

--- clientMobile/Home/UpdateProject.js
+++ clientMobile/Home/UpdateProject.js
@@ -74,6 +74,37 @@ class UpdateProjectForm extends Component {
         tempArr.push('');
       }
     }
+    
+    this._isMounted = true;
+
+    var response = await fetch(
+      'http://projecttree.herokuapp.com/project/projecttasks',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({projId: this.props.project.id}),
+      },
+    );
+
+    let firstTask = null;
+    let lastTask = null;
+
+    var body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    else {
+      if (this._isMounted === true) { 
+        body.tasks.forEach(task => {
+          if ( firstTask === null || firstTask.startDate > task.startDate )
+            firstTask = task;
+          if ( lastTask === null || lastTask.endDate < task.endDate )
+            lastTask = task;
+        });
+        this.setState({firstTask, lastTask});
+      }
+    }
 
     this.state = {
       projName: this.props.project.name,
@@ -83,8 +114,8 @@ class UpdateProjectForm extends Component {
       endDate: this.props.project.endDate,
       dateTimePicker: false,
       dateTimeType: {type: 'date', for: 'start', value: new Date()},
-      firstTask: null,
-      lastTask: null,
+      firstTask: firstTask,
+      lastTask: lastTask,
       error: null,
       dateTimePicker: false,
       dateTimeType: {type: 'date', for: 'start', value: new Date()},
