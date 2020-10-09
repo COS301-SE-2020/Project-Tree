b327a066d1ae6e6f2b1acb141e0f91774fbf1c15
Brandon Robinson <u18021353@tuks.co.za>
2020-10-09 11:09:39 +0200

    fixed update project if no tasks

--- clientWeb/src/Project/UpdateProject.js
+++ clientWeb/src/Project/UpdateProject.js
@@ -107,12 +107,12 @@ class UpdateProject extends React.Component {
       value : this.state.project.startDate;
     let endDate = type.substring(0,1) === "s" ? 
       this.state.project.endDate : value;
-      console.log(this.state.lastTask.endDate);
-    if (startDate > this.state.firstTask.startDate){
+
+    if (this.state.firstTask !== null && startDate > this.state.firstTask.startDate){
       alert("The start date of the project cannot be any latter then the current date as it would be after the start of a task, if you want it to be any later then change the task first");
       value = this.state.firstTask.startDate;
     }
-    if (endDate < this.state.lastTask.endDate){
+    if (this.state.lastTask !== null && endDate < this.state.lastTask.endDate){
       alert("The end date of the project cannot be any earlier then the current date as it would be before a end of a task, if you want it to be any earlier then change the task first");
       value = this.state.lastTask.endDate;
     }
